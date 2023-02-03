5de0595b528e88d4de14c9662742358d3fcd05d5
redrumjisu <redrumjisu@gmail.com>
2023-02-03 14:08:42 +0900

    Modify server.js
    - Add get, post

--- server.js
+++ server.js
@@ -1,16 +1,59 @@
-// 1. 서버 사용을 위해서 http 모듈을 http 변수에 담는다. (모듈과 변수의 이름은 달라도 된다.)
 var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
 
-// 2. http 모듈로 서버를 생성한다.
-//    아래와 같이 작성하면 서버를 생성한 후, 사용자로 부터 http 요청이 들어오면 function 블럭내부의 코드를 실행해서 응답한다.
 var server = http.createServer(function(request,response){
+    console.log(request.url);
+    var parsedUrl = url.parse(request.url);
+    var resource = parsedUrl.pathname;
+    console.log('resource path=%s',resource);
 
-    response.writeHead(200,{'Content-Type':'text/html'});
-    response.end('Hello node.js!!');
+    if (resource == '/get') {
 
+        console.log('--- log start ---');
+        var parsedUrl = url.parse(request.url);
+        console.log(parsedUrl);
+        var parsedQuery = querystring.parse(parsedUrl.query,'&','=');
+        console.log(parsedQuery);
+        console.log('--- log end ---');
+
+        for (key in parsedQuery) {
+            console.log(key + ' ' + parsedQuery[key]);
+        }
+
+        response.writeHead(200, {'Content-Type':'text/html'});
+        response.end('get - '  + JSON.stringify(parsedQuery));
+
+    } else if (resource == '/post') {
+        var postdata = '';
+        request.on('data', function (data) {
+            postdata = postdata + data;
+        });
+
+        request.on('end', function () {
+            console.log(postdata);
+
+            var parsedQuery = querystring.parse(postdata);
+            console.log(parsedQuery);
+
+            for (key in parsedQuery) {
+                console.log(key + ' ' + parsedQuery[key]);
+            }
+
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end('post - ' + JSON.stringify(parsedQuery));
+        });
+    } else {
+        if (resource == '/') {
+            response.writeHead(200,{'Content-Type':'text/html'});
+            response.end('Hello node.js!!');
+        } else {
+            response.writeHead(404, {'Content-Type':'text/html'});
+            response.end('404 Page Not Found');
+        }
+    }
 });
 
-// 3. listen 함수로 8080 포트를 가진 서버를 실행한다. 서버가 실행된 것을 콘솔창에서 확인하기 위해 'Server is running...' 로그를 출력한다
 server.listen(8080, function(){
     console.log('Server is running...');
 });
