21b26d9b4907b74d0a0eb025d5821584723c078e
redrumjisu <redrumjisu@gmail.com>
2023-02-06 17:55:49 +0900

    Add read images and install mime

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var url = require('url');
 var querystring = require('querystring');
 
 var fs = require('fs');
+var mime = require('mime');
 
 var server = http.createServer(function(request,response){
     console.log(request.url);
@@ -46,12 +47,27 @@ var server = http.createServer(function(request,response){
             response.end('post - ' + JSON.stringify(parsedQuery));
         });
     } else if (resource == '/hello') {
-        fs.readFile('hello.html', 'utf-8', function(error, data) {
+        fs.readFile('hello.html', 'utf-8', function (error, data) {
+            if (error) {
+                response.writeHead(500, {'Content-Type': 'text/html'});
+                response.end('500 Internal Server Error : ' + error);
+            } else {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.end(data);
+            }
+        });
+    } else if (resource.indexOf('/images/') == 0) {
+        var imgPath = resource.substring(1);
+        console.log('imgPath='+imgPath);
+        var imgMime = mime.getType(imgPath); // lookup -> getType으로 변경됨
+        console.log('mime='+imgMime);
+
+        fs.readFile(imgPath, function(error, data) {
             if(error){
                 response.writeHead(500, {'Content-Type':'text/html'});
-                response.end('500 Internal Server Error : '+error);
+                response.end('500 Internal Server '+error);
             }else{
-                response.writeHead(200, {'Content-Type':'text/html'});
+                response.writeHead(200, {'Content-Type':imgMime});
                 response.end(data);
             }
         });
