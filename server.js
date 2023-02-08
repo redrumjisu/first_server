d7278457168d9d42112a955e13a753d9185f1d35
redrumjisu <redrumjisu@gmail.com>
2023-02-08 15:26:20 +0900

    Add streaming service

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var server = http.createServer(function(request,response){
     var parsedUrl = url.parse(request.url);
     var resource = parsedUrl.pathname;
     console.log('resource path=%s',resource);
+    var resourcePath = '.' + resource
 
     if (resource == '/get') {
 
@@ -46,8 +47,10 @@ var server = http.createServer(function(request,response){
             response.writeHead(200, {'Content-Type': 'text/html'});
             response.end('post - ' + JSON.stringify(parsedQuery));
         });
-    } else if (resource == '/hello') {
-        fs.readFile('hello.html', 'utf-8', function (error, data) {
+    } else if (resource.indexOf('/htmls/') == 0) {
+        var htmlPath = resource.substring(1);
+        console.log('htmlPath='+htmlPath);
+        fs.readFile(resourcePath, 'utf-8', function (error, data) {
             if (error) {
                 response.writeHead(500, {'Content-Type': 'text/html'});
                 response.end('500 Internal Server Error : ' + error);
@@ -55,6 +58,32 @@ var server = http.createServer(function(request,response){
                 response.writeHead(200, {'Content-Type': 'text/html'});
                 response.end(data);
             }
+        });
+    } else if (resource.indexOf('/movies/') == 0) {
+        // 1. stream 생성
+        var stream = fs.createReadStream(resourcePath);
+        // 2. 잘게 쪼개진 stream 이 몇번 전송되는지 확인하기 위한 count
+        var count = 0;
+        // 3. 잘게 쪼개진 data를 전송할 수 있으면 data 이벤트 발생
+        stream.on('data', function(data) {
+            count = count + 1;
+            console.log('data count='+count);
+            // 3.1. data 이벤트가 발생되면 해당 data를 클라이언트로 전송
+            response.write(data);
+        });
+
+        // 4. 데이터 전송이 완료되면 end 이벤트 발생
+        stream.on('end', function () {
+            console.log('end streaming');
+            // 4.1. 클라이언트에 전송완료를 알림
+            response.end();
+        });
+
+        // 5. 스트림도중 에러 발생시 error 이벤트 발생
+        stream.on('error', function(err) {
+            console.log(err);
+            // 5.2. 클라이언트로 에러메시지를 전달하고 전송완료
+            response.end('500 Internal Server '+err);
         });
     } else if (resource.indexOf('/images/') == 0) {
         var imgPath = resource.substring(1);
